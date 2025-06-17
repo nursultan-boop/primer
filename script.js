a78f23fc3cd1cde136262458b1dfb21b60463c18
nursultan-boop <60815706+nursultan-boop@users.noreply.github.com>
2025-06-17 19:39:10 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
 // Ждем, пока весь HTML-документ будет загружен
 document.addEventListener('DOMContentLoaded', function() {
-
-    // Спрашиваем имя пользователя
-    const name = prompt("Как вас зовут?");
-
-    // Проверяем, ввел ли пользователь имя
-    if (name) {
-        // Находим элемент с id="welcome-message"
-        const welcomeElement = document.getElementById('welcome-message');
-        // Вставляем приветствие в этот элемент
-        welcomeElement.textContent = 'Привет, ' + name + '!';
-    }
+  
+  // Код из примера на слайде 14
+  alert("Hello, World!");
 
 });
